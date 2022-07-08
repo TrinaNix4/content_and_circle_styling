5763f521aca4eac389c04ee718d4cf45ade9a8dd
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-07-07 18:43:19 -0600

    when clicked it adds the class

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
+open.addEventListener("click");
